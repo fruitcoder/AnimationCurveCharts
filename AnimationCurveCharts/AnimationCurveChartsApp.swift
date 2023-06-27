3e7cf6f474833076b02fa9e2c97bef86fb611fec
Alexander Türk <alx91@me.com>
2023-06-27 12:08:15 +0200

    Initial Commit

---
+++ AnimationCurveCharts/AnimationCurveChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationCurveChartsApp.swift
+//  AnimationCurveCharts
+//
+//  Created by fruitcoder on 27.06.23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationCurveChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
